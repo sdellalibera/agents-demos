8b3fe14b2c0f8ebec269f6153b31977d8d57644e
Simone Della Libera <simonedellalibera0@gmail.com>
2025-12-11 18:31:44 +0100

    started first maf demo to show client adapter

---
+++ agent-framework/maf_demo1.cs
@@ -0,0 +1,24 @@
+#:package Azure.Identity@1.17.1
+#:package Azure.AI.OpenAI@2.1.0
+#:package Microsoft.Agents.AI@1.0.0-preview.251204.1
+#:package Microsoft.Extensions.AI.OpenAI@10.1.0-preview.1.25608.1
+
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+
+const string endpoint = "https://sdellalibera-foundry-demos.openai.azure.com/";
+
+var chat_client = new AzureOpenAIClient(new Uri(endpoint), new DefaultAzureCredential());
+
+IChatClient client_adapter = chat_client.GetChatClient("gpt-4.1").AsIChatClient();
+
+AIAgent writer = new ChatClientAgent(
+    chatClient: client_adapter,
+    instructions: "You are an helpful agent that writes book stories",
+    name: "WriterAgent"
+);
+
+
+
